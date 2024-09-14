95a2f2d6cff4f5d1cad8e3f6ddaed799cea4a3de
RiteshGadre <riteshgadre9@gmail.com>
2024-09-14 17:46:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+
+const App = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
+
+
+
+/*
+- Header
+  - Quiz App (Heading)
+- Main
+  - Pargaraph (we will add question here)
+  - radio button with 4 options
+
+
+  Once all 10 questions are attempted then we will do following thisng
+  - Result Componant
+    - Wrong Answer Count
+    - Right Answer Count
+    - Restart Quiz
+    - Exit
+*/
